127e017c65b7cc7cb2a5b49da7754495580db359
Helena <133242692+HellyGH@users.noreply.github.com>
2023-10-30 13:55:03 +0200

    Add files via upload

---
+++ Amazonka.java
@@ -0,0 +1,16 @@
+public class Amazonka extends Character{
+
+    public Amazonka(String name, int health, int power, String race) {
+        super(name, health, power, race);
+    }
+
+    @Override
+    public void attack(Monster target) {
+
+    }
+
+    @Override
+    public void defend(int attackPower) {
+
+    }
+}
